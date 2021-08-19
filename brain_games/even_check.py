7845396972ebfd20af153bbeab2aed19a70749a9
Lisa Nalyvaiko <y.nalyvaiko@gmail.com>
2021-08-19 21:07:30 +0300

    Add check for even game

---
+++ brain_games/even_check.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import prompt
+import random
+
+
+def even_number():
+    """Prompts user to guess the number.
+
+    Returns:
+        string and answer prompt.
+    """
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Welcome,{name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    tries = 0
+    while tries < 3:
+        number = random.randint(0, 100)
+        print(f'Question: {number}')
+        answer = prompt.string('Your answer: ')
+        if number % 2 == 0:
+            # print('number is even')
+            if answer.lower() == 'yes':
+                print('Correct!')
+                tries += 1
+                continue
+            else:
+                print(f"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}!")
+                break
+        else:
+            # print('number is odd')
+            if answer.lower() == 'yes':
+                print(f"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}!")
+                break
+            else:
+                print('Correct!')
+                tries += 1
+                continue
+    if tries == 3:
+        print(f'Congratulations, {name}!')
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from brain_games.even_check import even_number
+
+
+def main():
+    """Make game intro.
+
+    Returns:
+        string and user name prompt.
+    """
+    return even_number()
+
+
+if __name__ == '__main__':
+    main()
