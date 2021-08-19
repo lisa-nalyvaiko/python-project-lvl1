028f67c18373016f005764962206681521af39ab
Lisa Nalyvaiko <y.nalyvaiko@gmail.com>
2021-08-19 16:07:50 +0300

    Complete 3rd step

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python
+
+from brain_games.scripts.cli import welcome_user
+
 def main():
-    print("Welcome to the Brain Games!")
+    return(welcome_user())
 
 if __name__ == '__main__':
     main()
