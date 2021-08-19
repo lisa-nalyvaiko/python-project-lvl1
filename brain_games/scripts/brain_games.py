41d4f52cd2e0e8f86e03544084e897b0e9e55224
Lisa Nalyvaiko <y.nalyvaiko@gmail.com>
2021-08-19 17:36:52 +0300

    Set up linter

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
+"""Main script."""
+
 
 from brain_games.scripts.cli import welcome_user
 
+
 def main():
-    return(welcome_user())
+    """Make game intro.
+
+    Returns:
+        string and user name prompt.
+    """
+    return welcome_user()
+
 
 if __name__ == '__main__':
     main()
-
--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
+"""Script with welcome function."""
 import prompt
 
-def welcome_user():
-    print("Welcome to the Brain Games!")
-    name = prompt.string("May I have your name? ")
-    print(f"Welcome,{name}!")
 
+def welcome_user():
+    """Welcome message and user name prompt.
+
+    Returns: string and input.
+    """
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Welcome,{name}!')
