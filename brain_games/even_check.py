c67894dca016d39fe07e87d469858eb4b26bab5f
Lisa Nalyvaiko <y.nalyvaiko@gmail.com>
2021-08-19 21:16:07 +0300

    Fix linter errors

--- brain_games/even_check.py
+++ brain_games/even_check.py
@@ -26,13 +26,13 @@ def even_number():
                 tries += 1
                 continue
             else:
-                print("'no' is wrong answer ;(. " +
+                print("'no' is wrong answer ;(. " + \
                       f"Correct answer was 'yes'.\nLet's try again, {name}!")
                 break
         else:
             # print('number is odd')
             if answer.lower() == 'yes':
-                print("'yes' is wrong answer ;(. " +
+                print("'yes' is wrong answer ;(. " + \
                       f"Correct answer was 'no'.\nLet's try again, {name}!")
                 break
             else:
