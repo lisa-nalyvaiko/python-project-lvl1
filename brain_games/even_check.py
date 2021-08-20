f10525f3103176b0e69d402803fb22348bfc0e4b
Lisa Nalyvaiko <y.nalyvaiko@gmail.com>
2021-08-20 11:40:04 +0300

    optimize check-for-even logic

--- brain_games/even_check.py
+++ brain_games/even_check.py
@@ -13,29 +13,32 @@ def even_number():
     name = prompt.string('May I have your name? ')
     print(f'Welcome,{name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
-
-    tries = 0
-    while tries < 3:
-        number = random.randint(0, 100)
+    wrong = f"'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {name}!"
+    wrong_odd = f"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}!"
+    right = 'Correct!'
+    win = f'Congratulations, {name}!'
+    counter = 0
+    while counter < 3:
+        number = random.randint(1, 100)
         print(f'Question: {number}')
         answer = prompt.string('Your answer: ')
         if number % 2 == 0:
             # print('number is even')
             if answer.lower() == 'yes':
-                print('Correct!')
-                tries += 1
+                print(right)
+                counter += 1
                 continue
             else:
-                print(f"'no' is wrong answer ;(. Correct answer was 'yes'. Let's try again, {name}!")
+                print(wrong)
                 break
         else:
             # print('number is odd')
             if answer.lower() == 'yes':
-                print(f"'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, {name}!")
+                print(wrong_odd)
                 break
             else:
-                print('Correct!')
-                tries += 1
+                print(right)
+                counter += 1
                 continue
-    if tries == 3:
-        print(f'Congratulations, {name}!')
+    if counter == 3:
+        print(win)
