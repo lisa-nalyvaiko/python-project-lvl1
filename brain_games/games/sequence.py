d94e5fc52c97bf10cdd2c4989a60923a1787f20e
Lisa Nalyvaiko <y.nalyvaiko@gmail.com>
2021-08-21 15:46:33 +0300

    Fix linter errors.

--- brain_games/games/sequence.py
+++ brain_games/games/sequence.py
@@ -8,10 +8,10 @@ def game_question():
     step = random.randint(1, 10)
     length = 10
     sequence = [start]
-    for i in range(length-1):
-        sequence.append(sequence[-1]+step)
+    for i in range(length - 1):
+        sequence.append(sequence[-1] + step)
     guess = random.randint(1, 10)
-    result = str(sequence[guess-1])
-    sequence[guess-1] = '..'
+    result = str(sequence[guess - 1])
+    sequence[guess - 1] = '..'
     problem = ', '.join(map(str, sequence))
     return problem, result
